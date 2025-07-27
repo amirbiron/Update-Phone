16c69649d1477ae3894e692703e16f543006eecc
amir haim <amirbiron@gmail.com>
2025-07-27 06:21:44 +0300

    Create userService.js

---
+++ services/userService.js
@@ -0,0 +1,54 @@
+const { getDB } = require('./mongo');
+
+// A helper function to get the users collection
+const getUsersCollection = () => {
+    const db = getDB();
+    if (!db) {
+        throw new Error('Database not connected. Cannot get users collection.');
+    }
+    return db.collection('users');
+};
+
+/**
+ * Finds a user by their Telegram ID. If the user doesn't exist, it creates a new one.
+ * @param {object} telegramUser - The user object from the Telegram message (msg.from).
+ * @returns {Promise<object>} The user document from the database.
+ */
+async function getOrCreateUser(telegramUser) {
+    const users = getUsersCollection();
+    const { id, first_name, last_name, username } = telegramUser;
+
+    let user = await users.findOne({ telegramId: id });
+
+    if (!user) {
+        console.log(`User with ID ${id} not found. Creating new user...`);
+        const newUser = {
+            telegramId: id,
+            firstName: first_name,
+            lastName: last_name,
+            username: username,
+            monthlyQueryCount: 0,
+            firstSeen: new Date(),
+        };
+        await users.insertOne(newUser);
+        user = newUser;
+    }
+
+    return user;
+}
+
+/**
+ * Updates the monthly query count for a specific user.
+ * @param {number} telegramId - The user's Telegram ID.
+ * @param {number} newCount - The new query count to set.
+ * @returns {Promise<void>}
+ */
+async function updateUserQueries(telegramId, newCount) {
+    const users = getUsersCollection();
+    await users.updateOne(
+        { telegramId: telegramId },
+        { $set: { monthlyQueryCount: newCount, lastQueryDate: new Date() } }
+    );
+}
+
+module.exports = { getOrCreateUser, updateUserQueries };
